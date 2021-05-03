3a8e0e9d9047a714ca72358d8754665a91d7c65e
Maxwell Garrett <maxwell.garrett@mail.utoronto.ca>
2021-05-03 19:36:17 -0400

    Adding a home page seperate from App.js

---
+++ Home.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import TopBanner from './TopBanner';
+import RowElement from './RowElement';
+
+const Home = (props) => {
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle='light-content'/>
+      <TopBanner titleName='Blue'/>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <RowElement rowName="Time since out im too long" countdown={false}/>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default Home;
