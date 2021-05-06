67b417e14d10e4d2e374b934ccab26d235a5158e
Maxwell Garrett <maxwell.garrett@mail.utoronto.ca>
2021-05-06 12:42:00 -0400

    Created storage functions to handle Async storage of data

---
+++ Storage.js
@@ -0,0 +1,61 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value)
+    await AsyncStorage.setItem('rows', jsonValue)
+  } catch (e) {
+    // saving error
+    console.log(value)
+  }
+}
+
+const resetTimer = async (uuid, callback) => {
+  getData('rows').then(
+    (data) => JSON.parse(data)
+  ).then(
+    (data) => {
+      const index = data.findIndex((obj) => uuid == obj.uuid)
+      data[index].initialStart = Date.now();
+      console.log(data[index])
+      callback(data[index])
+      storeData(data)
+    })
+}
+
+const incrementCounter = async (uuid, callback) => {
+  getData('rows').then(
+    (data) => JSON.parse(data)
+  ).then(
+    (data) => {
+      const index = data.findIndex((obj) => uuid == obj.uuid)
+      data[index].numEmojis += 1
+      callback(data[index])
+      storeData(data)
+    }
+  )
+}
+
+const deleteRow = async (uuid, changeObj) => {
+  getData('rows').then(
+    (data) => JSON.parse(data)
+  ).then(
+    (data) => {
+      rows.splice(data.findIndex((obj) => uuid == obj.uuid),1)
+      storeData(rows)
+    }
+  )
+}
+
+const getData = async (key) => {
+  try {
+    const jsonValue = AsyncStorage.getItem('rows')
+    /*jsonValue != null ? changeRows(JSON.parse(jsonValue)) : null; */
+    return jsonValue
+  } catch(e) {
+    // error reading value
+    console.log("Error reading value")
+  }
+}
+
+export {storeData, getData, resetTimer, incrementCounter, deleteRow};
